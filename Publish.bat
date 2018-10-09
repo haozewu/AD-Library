6bd5a70d69650832aa82cc83648fd2eec527356b
吴昊泽 <haozewu@outlook.com>
2018-10-09 23:22:00 +0800

    update some lib and add bat
    修整了RES，添加了几个元件和发布批处理指令。

---
+++ Publish.bat
@@ -0,0 +1,10 @@
+@echo delete files 
+cd Integrated Library\History
+del /s /q *.Zip
+cd ..\Project Outputs for Library
+del /s /q *.IntLib
+cd ..\..\Demo\History
+del /s /q *
+cd ..\Project Logs for Demo
+del /s /q *
+pause
