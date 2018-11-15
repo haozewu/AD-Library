a8c991f1e0b142109c8c591faad8e330542a3cf7
haozewu <haozewu@outlook.com>
2018-11-15 18:48:31 +0800

    make usb useable
    MICRO USB封装已经进行调整

--- Publish.bat
+++ Publish.bat
@@ -3,7 +3,10 @@ cd Integrated Library\History
 del /s /q *.Zip
 cd ..\Project Outputs for Library
 del /s /q *.IntLib
-cd ..\..\Demo\History
+cd ..\
+del /s /q debug.log
+
+cd ..\Demo\History
 del /s /q *
 cd ..\Project Logs for Demo
 del /s /q *
